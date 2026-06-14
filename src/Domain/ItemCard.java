f84c9b0414be1f5cc216e5e977a40b2c6eeb5dbe
Felipe Olivares <felipe.olivares04@alumnos.ucn.cl>
2026-06-14 16:14:04 -0400

    Add javadoc to Item and Pokemon cards

--- src/Domain/ItemCard.java
+++ src/Domain/ItemCard.java
@@ -2,12 +2,26 @@ package domain;
 
 public class ItemCard extends Card {
 	private int bonus;
+	
+    /**
+     * Creates a new item card.
+     *
+     * @param cardName the name of the card
+     * @param rarity the rarity level of the card
+     * @param bonus the bonus value provided by the item
+     */
 
-	public ItemCard(String cardName, int rarity, String type, int bonus) {
+	public ItemCard(String cardName, int rarity, int bonus) {
 		super(cardName, rarity, "Item");
 		this.bonus = bonus;
 	}
 
+	
+    /**
+     * Calculates the power of this item card.
+     *
+     * @return the bonus multiplied by 20
+     */
 	@Override
 	public int getPower() {
 		return bonus * 20;
--- src/Domain/PokemonCard.java
+++ src/Domain/PokemonCard.java
@@ -2,12 +2,29 @@ package domain;
 
 public class PokemonCard extends Card {
 	private int damage, energy;
-
+	
+    /**
+     * Creates a new Pokémon card.
+     *
+     * @param cardName the name of the card
+     * @param rarity the rarity level of the card
+     * @param damage the damage dealt by the Pokémon
+     * @param energy the energy required to use its attack
+     */
 	public PokemonCard(String cardName, int rarity, int damage, int energy) {
 		super(cardName, rarity, "Pokemon");
 		this.damage = damage;
 		this.energy = energy;
 	}
+	
+
+    /**
+     * Calculates the power of this Pokémon card.
+     * The power is determined by the ratio of damage to
+     * energy cost, multiplied by 100.
+     *
+     * @return the calculated power value
+     */
 
 	@Override
 	public int getPower() {
