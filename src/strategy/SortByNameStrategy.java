186a2f3f96210dee6e7a58789223db9ebeed89c5
FelipeOlivares-ICCI-UCN <felipe.olivares04@alumnos.ucn.cl>
2026-06-23 15:24:45 -0400

    Add strategy package with methods and interfaces  based on strategy pattern

---
+++ src/strategy/SortByNameStrategy.java
@@ -0,0 +1,49 @@
+package strategy;
+
+import java.util.List;
+
+import domain.Card;
+
+
+public class SortByNameStrategy  implements SortingStrategy {
+
+	@Override
+	public void sortCollection(List<Card> c) {
+		if (c.size() <= 1 || c == null) 
+		{
+			return;
+		}
+		
+		quickSort(c, 0, c.size());
+		
+	}
+	public static void quickSort(List<Card> c, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(c, low, high);
+            quickSort(c, low, pivotIndex - 1);  // Sort left half
+            quickSort(c, pivotIndex + 1, high); // Sort right half
+        }
+    }
+
+    private static int partition(List<Card> c, int low, int high) {
+        String pivot = c.get(high).getCardName(); 
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (c.get(j).getCardName().compareTo(pivot) <= 0) {
+                i++;
+                swap(c, i, j);
+            }
+        }
+        swap(c, i + 1, high);
+        return i + 1;
+    }
+
+    private static void swap(List<Card> c, int i, int j) 
+    {
+    	Card temp = c.get(i);
+    	c.set(i, c.get(j));
+    	c.set(j, temp);
+    	
+    }   	
+}
---
+++ src/strategy/SortingStrategy.java
@@ -0,0 +1,10 @@
+package strategy;
+
+import java.util.List;
+
+import domain.Card;
+
+public interface SortingStrategy {
+	void sortCollection(List<Card> c);
+
+}
