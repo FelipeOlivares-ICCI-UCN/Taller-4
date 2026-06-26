5febecfa58179460ba95d3833450b7a507949a2e
FelipeOlivares-ICCI-UCN <felipe.olivares04@alumnos.ucn.cl>
2026-06-26 10:18:22 -0400

    Add new Class for selecting the card type

---
+++ src/gui/DropdownCardSelection.java
@@ -0,0 +1,33 @@
+package gui;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import domain.*;
+public class DropdownCardSelection {
+	public Card createCardMenu(JFrame main) {
+	
+	
+	String[] choices = {"Pokemon", "Supporter", "Energy", "Item"};
+	
+	String selected = (String) JOptionPane.showInputDialog(
+			main,
+			"Select the type of your new card",
+			"Card Type",
+			JOptionPane.QUESTION_MESSAGE,
+			null,
+			choices,
+			choices[0]
+		);
+	if (selected.equals("Pokemon")) return new PokemonCard("", 0, 0, 0);  
+	
+	if (selected.equals("Supporter")) return new SupporterCard("", 0, 0);
+	
+	if (selected.equals("Energy")) return new EnergyCard("", 0, "");
+	
+	if (selected.equals("Item")) return new ItemCard("", 0, 0);
+	
+	return null;
+	
+	}
+}
